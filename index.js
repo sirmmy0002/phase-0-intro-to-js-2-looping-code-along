6ddafc40d89fa5aabb0738ed40d7f324cb192d1a
sirmmy0002 <sammyndungu52@gmail.com>
2022-06-05 08:40:22 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(namesArray,event) {
+    let thankYouCards = [];
+    for (let i = 0; i < namesArray.length; i++){
+        thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouCards;
+}
+function countDown (num){
+    while(num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+}
